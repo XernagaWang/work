92b6c9346ca13562367cac114456beec77c82e7c
XernagaWang <j20010320@gmail.com>
2023-05-13 14:19:47 +0800

    fix app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
+
 print('Hello VSOCDE')
-=======
+
 str1 = 'python'
 # str1 = 'nothing ganna change'
 
 i = 0
 
 while i < len(str1): 
-    # print(i)
-    # print(str1[-i], end= '')
-    # print(i)
     print(str1[-i], end = '')
     i += 1
->>>>>>> 405b0a5 (1st commit)
